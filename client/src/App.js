36ec61b068948fa22ea1c3efd3989f857317a9b9
Salac <salacfrantisek@seznam.cz>
2023-10-19 22:13:41 +0200

    adding custom react content to client folder

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>*/
+      <center>
+      <div>
+        <h1>Jetbraini aktualizovali intelliJ pro linux</h1>
+          <h2>Rozbili mi to</h2>
+          <h3>nic nefunguje</h3>
+            <h4>achjo</h4>
+
+      </div>
+      </center>
+  );
+}
+
+export default App;
